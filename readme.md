4155ab90d9a30296c387881f27d28ffd7c9514f9
BuildTools <unconfigured@null.spigotmc.org>
2021-08-13 12:16:03 +0800

    Finished

---
+++ readme.md
@@ -0,0 +1,25 @@
+# Nova Maven Deployer
+## Allows you to deploy minecraft's libraries into maven fast & safe
+### Usage:
+0. You ***can't*** use it **Directly** (Means you can still use it, but you need to change something). All dependencies have been deployed to our Nova Maven Repository (It's private tho lol) , you can use them now by connecting to our VPN (VPN, virtual private network. So it's private :D). The only way you can use it is by modifying the code. Since it's WTFPL. You can make money with it, but I don't know how can you make money with this stupid script lol.
+
+### Really? What if I really want to use this script? (Since MavenMCP repositories are broken)
+You can just change a single value, but I don't know what did I code.<br>
+Since the code is pretty short and lightweight, you can just read the code.<br>
+You can probably just change "/home/fan87/.minecraft/libraries" to your .minecraft/libraries, and also change your repository name by changing deploy file command.<br>
+You'll want to learn Maven Deploy Plugin first, by adding a private repository, you can go ~/.m2 and modify a file called settings.xml. For more information, please check maven document.<br>
+
+### License
+[***Do what the fuck you want to public license (WTFPL)***](http://www.wtfpl.net/) since I made this in like 2 minutes or something<br>
+I don't care if it's skidded, cuz anyone can make this in 2 minutes I guess.<br>
+Oh, btw, the time im writing readme.md is even longer than coding. Blyat.
+
+
+### About the script
+"Yes, do what I said." is from apt remove or something like that.<br>
+Never do `apt remove python3`<br>
+It will kill your computer in 10 seconds.<br>
+It's not worth it unless you got virtual machine<br>
+I was dumb and thought you know what let's remove python3<br>
+and boom my computer is gone<br>
+the only way you can save this computer is by re-installing entire system<br>
--- src/main/java/club/novaclient/mavendeploier/Main.java
+++ src/main/java/club/novaclient/mavendeploier/Main.java
@@ -1,9 +1,89 @@
 package club.novaclient.mavendeploier;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static final ArrayList<Artifact> artifacts = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File root = new File("/home/fan87/.minecraft/libraries");
+        System.out.println("Main Path: " + root.getAbsolutePath());
+        System.out.println("WARNING: DOING IT MIGHT FUCK YOUR MAVEN REPO UP, YOU SURE? (it's for personally use mainly, but you can modify the code to get this work)");
+        Scanner input = new Scanner(System.in);
+        System.out.println("If you understand what you are doing, and you modified the code, and double checked it, please type:");
+        System.out.println("\"Yes, do what I said.\"");
+        if (!input.nextLine().equals("Yes, do what I said.")) {
+            System.out.println("Okay. Process ended. Press ENTER to quit.");
+            input.nextLine();
+            System.exit(0);
+            return;
+        }
+        System.out.println("Indexing...");
+        indexFolder(root);
+        for (Artifact artifact : artifacts) {
+            System.out.println("Deploying Artifact: " + artifact.getName());
+            Process process = Runtime.getRuntime().exec(
+                    "mvn deploy:deploy-file -DgroupId=<group-id> -DartifactId=<artifact-id> -Dversion=<version> -Dpackaging=jar -Dfile=<path-to-file> -DrepositoryId=nova -Durl=http://69.69.0.1:8081/repository/nova-repository/"
+                            .replaceAll("<group-id>", artifact.getGroupID())
+                            .replaceAll("<artifact-id>", artifact.getArtifactID())
+                            .replaceAll("<version>", artifact.getVersion())
+                            .replaceAll("<path-to-file>", artifact.getPath().getAbsolutePath()));
+            Scanner scanner = new Scanner(process.getInputStream());
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        }
+    }
 
+    public static void indexFolder(File folder) {
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                System.out.println("Found child folder: " + file.getName());
+                indexFolder(file);
+            }
+            if (!file.isDirectory() && file.getName().endsWith("jar") && !file.getName().equalsIgnoreCase("MavenDeployer-1.0-SNAPSHOT.jar")) {
+                Artifact artifact = new Artifact(file);
+                artifacts.add(artifact);
+                System.out.println("Found artifact: " + artifact.getName() + " - " + artifact.getPath().getName());
+            }
+        }
+    }
+
+    public static class Artifact {
+        private File path;
+
+        public String getName() {
+            return getGroupID() + ":" + getArtifactID() + ":" + getVersion();
+        }
+
+        public Artifact(File path) {
+            this.path = path;
+        }
+
+        public File getPath() {
+            return path;
+        }
+
+        public String getArtifactID() {
+            return path.getParentFile().getParentFile().getName();
+        }
+        public String getGroupID() {
+            File parent = path.getParentFile().getParentFile().getParentFile();
+            String groupId = "";
+            while (!parent.getName().equalsIgnoreCase("libraries")) { // Track down to libraries folder, or root folder
+                groupId = parent.getName() + "." + groupId;
+                parent = parent.getParentFile();
+            }
+            groupId = groupId.substring(0, groupId.length() - 1);
+            return groupId;
+        }
+        public String getVersion() {
+            return path.getParentFile().getName();
+        }
     }
 
 }
